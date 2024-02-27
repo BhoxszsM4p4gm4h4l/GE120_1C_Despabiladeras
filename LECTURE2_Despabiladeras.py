e8ac2211c9bf582e575393c9b9019934ef79db71
Mariole Despabiladeras <mbdespabiladeras@up.edu.ph>
2024-02-27 16:17:34 +0800

    wow

---
+++ LECTURE2_Despabiladeras.py
@@ -0,0 +1,28 @@
+print("kamusta mundo xP")
+print(type("kumzta mundo.."))
+print(type(str(1)))
+print(type("1+j"))
+print(type(float(54)))
+print(type(3.2938923))
+print(type(1+4j)) 
+# comment ulol
+
+print("wazzup" +" ge120")
+x = 2
+y=13
+print("sum of x+y = ", x + y)
+print("diff of x-y = ", x - y)
+print("product of xy = ", x * y)
+print("quotient of x/y = ", x / y)
+print("quotient of x/y = ", x // y)
+print(x**y)
+print(x%y)
+print(abs(y))
+print(float(y))
+print(complex(y))
+print(divmod(y,x))
+
+#list is square bracket
+dog = ["golden retriever","husky","aspin"]
+print(dog)
+print(dog[0])
